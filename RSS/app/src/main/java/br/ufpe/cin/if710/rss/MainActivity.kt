7eb475900ac746c58ce8c6f69fad9314bb13b12b
menezesmalu <mlmv@cin.ufpe.br>
2018-09-11 11:39:38 -0300

    passos 2 e 3 - funcionando no cin

--- RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivity.kt
+++ RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivity.kt
@@ -1,15 +1,20 @@
 package br.ufpe.cin.if710.rss
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.os.AsyncTask
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import kotlinx.coroutines.experimental.async
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.UnknownHostException
 
-abstract class MainActivity : Activity() {
+class MainActivity : Activity() {
     //ao fazer envio da resolucao, use este link no seu codigo!
     private val RSS_FEED = "http://leopoldomt.com/if1001/g1brasil.xml"
     //OUTROS LINKS PARA TESTAR...
@@ -30,38 +35,46 @@ abstract class MainActivity : Activity() {
     override fun onStart(){
         super.onStart()
         try{
-            //Esse código dá pau, por fazer operação de rede na thread principal...
-            val feedXML = getRssFeed(RSS_FEED)
-            conteudoRSS.setText(feedXML)
+            loadRSS().execute(RSS_FEED)
         } catch(e: IOException) {
             e.printStackTrace()
         }
     }
 
     //Opcional - pesquise outros meios de obter arquivos da internet - bibliotecas, etc.
-    @Throws(IOException::class)
-    private fun getRssFeed(feed:String):String {
-        var in_: InputStream? = null
-        var rssFeed = ""
-        try{
-            val url= URL(feed)
-            val conn: HttpURLConnection = url.openConnection() as HttpURLConnection
-            in_ = conn.getInputStream()
-            val out = ByteArrayOutputStream()
-            val buffer = ByteArray(1024)
-            var count:Int = in_.read(buffer)
-            while (count != -1) {
-                out.write(buffer, 0, count)
-                count = in_.read(buffer)
+    @SuppressLint("StaticFieldLeak")
+    internal inner class loadRSS: AsyncTask<String, Void, String>(){
+        override fun doInBackground(vararg feed: String): String {
+            var in_: InputStream? = null
+            var rssFeed = ""
+            try {
+                val url = URL(feed[0])
+                val conn: HttpURLConnection = url.openConnection() as HttpURLConnection
+                in_ = conn.getInputStream()
+                val out = ByteArrayOutputStream()
+                val buffer = ByteArray(1024)
+                var count: Int = in_.read(buffer)
+                while (count != -1) {
+                    out.write(buffer, 0, count)
+                    count = in_.read(buffer)
+                }
+                val response = out.toByteArray()
+                rssFeed = String(response, charset("UTF-8"))
+            } catch (e: IOException) {
+                e.printStackTrace()
+                return "Sem Internet"
+            }finally {
+                if(in_ != null) {
+                    in_.close()
+                }
             }
-            val response = out.toByteArray()
-            rssFeed = String(response, charset("UTF-8"))
+            return rssFeed
 
-        }finally {
-            if(in_ != null) {
-                in_.close()
-            }
         }
-        return rssFeed
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            conteudoRSS.setText(result)
+        }
     }
 }
